0354ad3f5307b8364cfc542450e45a2144efe475
Xenon Thong <xenon.thong@ogilvy.com>
2017-04-13 18:42:51 +0800

    Initial commit

---
+++ src/js/bundle.js
@@ -0,0 +1,5 @@
+window.$ = window.jQuery = require('jquery')
+
+require('bootstrap-sass/assets/javascripts/bootstrap/dropdown')
+require('bootstrap-sass/assets/javascripts/bootstrap/transition')
+require('bootstrap-sass/assets/javascripts/bootstrap/collapse')
