3d0bf0aa697d9d1fffe0a5c13ba587f2414b17e0
vagrant <vagrant@homestead>
2017-04-17 09:53:53 +0000

    WIP - standard page

--- src/js/bundle.js
+++ src/js/bundle.js
@@ -1,5 +1,8 @@
 window.$ = window.jQuery = require('jquery')
+window.niceSelect = require('jquery-nice-select')
 
 require('bootstrap-sass/assets/javascripts/bootstrap/dropdown')
 require('bootstrap-sass/assets/javascripts/bootstrap/transition')
 require('bootstrap-sass/assets/javascripts/bootstrap/collapse')
+
+$('select').niceSelect()
--- webpack.mix.js
+++ webpack.mix.js
@@ -13,6 +13,7 @@ let mix = require('laravel-mix');
 
 mix.js('src/js/app.js', 'dist/')
    .sass('src/scss/app.scss', 'dist/')
+   .sourceMaps()
    .options({
       processCssUrls: false,
    });
